1c9f4f0d9123bc3c98272efdace8771e487cb56d
STP5940 <sitthipong114@gmail.com>
2024-05-02 14:01:19 +0700

    add elysia-rate-limit 500 req/minute

--- apps/backend/src/app/api/groupRoute.ts
+++ apps/backend/src/app/api/groupRoute.ts
@@ -1,8 +1,21 @@
+import { rateLimit } from "elysia-rate-limit";
+
 import { route as UserRoute } from "./user/route";
 import { route as TestRoute } from "./test/route";
 import { CreateElysia } from "../../utils/elysia";
 
 const route = CreateElysia()
+  .use(
+    rateLimit({
+      scoping: "local",
+      duration: 60 * 1000, // 1 minute in milliseconds
+      max: 500, // 500 requests per minute
+      responseCode: 429,
+      responseMessage: { status: "error", response: "rate-limit reached" },
+      generator: async (req, server, { ip }: { ip: string }) =>
+        Bun.hash(JSON.stringify(ip)).toString(),
+    })
+  )
   .use(TestRoute)
   .group("/v1", (app) => app.use(UserRoute));
 export { route as APIRoute };
