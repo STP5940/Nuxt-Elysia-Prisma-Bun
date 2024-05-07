47db66507f8a2d2aaf358fe0762d275a3f82bd5f
STP5940 <sitthipong114@gmail.com>
2024-05-07 13:41:36 +0700

    Add SECRET_KEY .env.example

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -1,11 +1,12 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 const envSchema = z.object({
-  ELYSIA_VERSION: z.string().default('0.0.0'),
-  ELYSIA_API_URL: z.string().default('http://localhost'),
-  PORT: z.string().default('3000'),
-  RUNTIME: z.enum(['bun', 'edge']).default('bun'),
-  NODE_ENV: z.enum(['development', 'production']).default('development')
-})
+  ELYSIA_VERSION: z.string().default("0.0.0"),
+  ELYSIA_API_URL: z.string().default("http://localhost"),
+  PORT: z.string().default("3000"),
+  RUNTIME: z.enum(["bun", "edge"]).default("bun"),
+  NODE_ENV: z.enum(["development", "production"]).default("development"),
+  SECRET_KEY: z.string().default("your_default_secret_key_here"),
+});
 
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse(process.env);
